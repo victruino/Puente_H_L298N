09a18ad668400dba5a5e0ba7f98700c5ca59b74e
victruino <40527382+victruino@users.noreply.github.com>
2018-07-18 03:10:54 -0500

    Add files via upload

---
+++ PH_L298N.ino
@@ -0,0 +1,36 @@
+// Entradas de Motores
+int M11 = 6; //IN1
+int M12 = 7; //IN2
+int M21 = 8; //IN3
+int M22 = 9; //IN4
+
+
+void setup() {
+  // Configuramos los pines
+  pinMode(M11,OUTPUT);
+  pinMode(M12,OUTPUT);
+  pinMode(M21,OUTPUT);
+  pinMode(M22,OUTPUT);
+
+  
+}
+
+void loop() {
+  
+  //motores giran en un sentido,
+  for(int i=0;i<3;i++){
+  digitalWrite(M11,HIGH);
+  digitalWrite(M12,LOW);
+  digitalWrite(M21,HIGH);
+  digitalWrite(M22,LOW);
+  delay(1000);}
+
+//motores giran al otro sentido
+    for(int j=0;j<3;j++){
+  digitalWrite(M11,LOW);
+  digitalWrite(M12,HIGH);
+  digitalWrite(M21,LOW);
+  digitalWrite(M22,HIGH);
+  delay(1000);}
+
+}
---
+++ PH_L298N_PWM.ino
@@ -0,0 +1,44 @@
+// Entradas de Motores
+int M11 = 6; //IN1
+int M12 = 7; //IN2
+int M21 = 8; //IN3
+int M22 = 9; //IN4
+
+//PWM para variar la velocidad de los motores
+int ENA = 3; //ENA
+int ENB = 11; //ENB
+
+// Variable de velocidad
+int spm;
+
+
+void setup() {
+  // Configuramos los pines
+  pinMode(M11,OUTPUT);
+  pinMode(M12,OUTPUT);
+  pinMode(M21,OUTPUT);
+  pinMode(M22,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  
+}
+
+void loop() {
+  //Variando la velocidad
+
+  digitalWrite(M11,HIGH);
+  digitalWrite(M12,LOW);
+  digitalWrite(M21,HIGH);
+  digitalWrite(M22,LOW);
+  delay(500);
+  
+for(spm=5;spm<255;spm+=10){
+  analogWrite(ENA,spm);
+  analogWrite(ENB,spm);
+  delay(100);}
+for(spm=255;spm>5;spm-=10){
+  analogWrite(ENA,spm);
+  analogWrite(ENB,spm);
+  delay(500);}
+
+}
